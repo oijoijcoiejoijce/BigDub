21e5c58af1c51d8c2117bbc4082294504ec59537
Jack Saunders <36443883+jsaunders909@users.noreply.github.com>
2023-04-12 10:06:53 +0100

    Script to run all individual model training

---
+++ train_many_hex.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import subprocess
+
+pairs = [
+    ("M009", 30),
+    ("M030", 3), ("M030", 5), ("M030", 10), ("M030", 30),
+    ("W011", 1), ("W011", 3), ("W011", 5), ("W011", 10), ("W011", 30)
+]
+
+for pair in pairs:
+    ID, n_vid = pair
+    cmd = f"{sys.executable} NeuralRendering/model.py --config configs/Hex.ini --restrict_to_ID {ID} --max_vid_idx {n_vid}"
+    subprocess.call(cmd, shell=True)
